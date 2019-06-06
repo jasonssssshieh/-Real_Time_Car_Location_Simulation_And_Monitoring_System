5e73957c86c35748f799c0f27a9ba052e0e94a0e
Z. Shieh <jasonssssshieh@users.noreply.github.com>
2019-06-06 16:02:08 -0400

    Add files via upload
    warm-up practices :-0

---
+++ week2-1/src/main/java/HelloWorldRestController.java
@@ -0,0 +1,10 @@
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldRestController {
+    @GetMapping("/")
+    public String helloWorld() {
+        return "Hellow Spring Boot";
+    }
+}
