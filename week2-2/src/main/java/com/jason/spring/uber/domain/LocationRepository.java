f25a9ffeb86495bad14bd0bebb15020d7a8245b9
Z. Shieh <jasonssssshieh@users.noreply.github.com>
2019-06-10 19:47:37 -0400

    Update LocationRepository.java

--- week2-2/src/main/java/com/jason/spring/uber/domain/LocationRepository.java
+++ week2-2/src/main/java/com/jason/spring/uber/domain/LocationRepository.java
@@ -8,7 +8,9 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RestResource;
 
 public interface LocationRepository extends JpaRepository<Location, Long> {
+    @RestResource(rel = "by-service-type")
     Page<Location> findByVehicleMovementType(@Param("movementType") Location.VehicleMovementType movementType, Pageable pageable);
+    @RestResource(path="vin", rel = "by-vin")
     Page<Location> findByUnitInfoUnitVin(@Param("unitVin") String unitVin, Pageable pageable);
     //因为unit vin是在一个embedable的 Unitinfo下的,所以我们第一层应该是UnitInfo, 然后才是UnitVin 不然我们spring data是没法找到匹配的
 }
